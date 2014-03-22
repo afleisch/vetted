37ac9230aff80ea3d0b3387984c358cf4c9be2e4
afleisch <afleisch@gmail.com>
2014-03-22 16:06:17 -0700

    homepage redirects to user profile or sign up page

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,4 +1,10 @@
 class SitesController < ApplicationController
   def index
+    if user_signed_in?
+      redirect_to profile_path(current_user.id)
+    else
+      redirect_to new_user_registration_path
+    end
+
   end
 end
