7464cd39daf92fbec1c238f74d07bcb8135c7ff5
afleisch <afleisch@gmail.com>
2014-03-22 16:51:27 -0700

    corrected routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Vetted::Application.routes.draw do
   devise_for :users
   root to: "sites#index"
-  resources :things, :profiles, only:[:show]
+  resources :things
+  resources :profiles, only:[:show]
 end
