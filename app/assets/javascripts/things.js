082c1d364e590634a86fcb35f8199ebbb21ad2d5
afleisch <afleisch@gmail.com>
2014-03-23 15:57:12 -0700

    added marker to googlemap

--- app/assets/javascripts/things.js
+++ app/assets/javascripts/things.js
@@ -7,6 +7,11 @@ function initialize() {
    };
   var map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(latitude, longitude),
+    map: map 
+  });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
