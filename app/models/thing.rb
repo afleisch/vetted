b1a0822d5c7e4c49f8fa0c9a2daa3a07f95796bd
afleisch <afleisch@gmail.com>
2014-03-26 14:55:18 -0700

    added image field to thing model

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -7,7 +7,9 @@ class Thing < ActiveRecord::Base
   #zip codes have to be 5 digits
   validates_length_of :zip_code, is: 5, :on => :create
   validates :name, :category, :why, :street, :city, :state, :zip_code, presence: true
-
+  has_attached_file :image, styles: {thumb: "60x60#"}
+  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
+  
   def address
   [street, city, state, zip_code].compact.join(', ')
   end
