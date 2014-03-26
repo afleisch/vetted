75c4bca4698dc0de88fa2cb311a22d324bb25676
afleisch <afleisch@gmail.com>
2014-03-26 12:01:26 -0700

    added basic validation to thing model

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -2,6 +2,11 @@ class Thing < ActiveRecord::Base
   belongs_to :user
   geocoded_by :address
   after_validation :geocode
+  #only use US state abbreviations
+  validates_length_of :state, is: 2, :on => :create
+  #zip codes have to be 5 digits
+  validates_length_of :zip_code, is: 5, :on => :create
+  validates :name, :category, :why, :street, :city, :state, :zip_code, presence: true
 
   def address
   [street, city, state, zip_code].compact.join(', ')
