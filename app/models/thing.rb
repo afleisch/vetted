452c45de96e6681152aa6b354e468d6f4e744dc0
afleisch <afleisch@gmail.com>
2014-03-26 15:42:16 -0700

    made zip code an optional field

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -4,12 +4,12 @@ class Thing < ActiveRecord::Base
   after_validation :geocode
   #only use US state abbreviations
   validates_length_of :state, is: 2, :on => :create
-  #zip codes have to be 5 digits
-  validates_length_of :zip_code, is: 5, :on => :create
-  validates :name, :category, :why, :street, :city, :state, :zip_code, presence: true
+  #zip codes have to be 5 digits if present
+  validates_length_of :zip_code, is: 5, :on => :create, :allow_blank => :true
+  validates :name, :category, :why, :street, :city, :state, presence: true
 
   def address
-  [street, city, state, zip_code].compact.join(', ')
+  [street.titleize, city.titleize, state.upcase, zip_code].compact.join(', ')
   end
 
 end
